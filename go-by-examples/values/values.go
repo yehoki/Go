f1171e2e7cbc67b41bae8b0bd5933d20f7190f2a
yehoki <patsenn2000@gmail.com>
2025-12-16 19:53:34 +0000

    Setup values

---
+++ go-by-examples/values/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("2+3 is ", 2+3)
+	fmt.Print("2132131")
+	fmt.Println(false)
+	fmt.Println(7.0 / 3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
