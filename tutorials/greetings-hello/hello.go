749a52dbce97b8c8331f13f99ddff810f5c2e747
yehoki <patsenn2000@gmail.com>
2025-12-17 22:53:28 +0000

    Add random strings for messaging

--- tutorials/greetings-hello/hello.go
+++ tutorials/greetings-hello/hello.go
@@ -17,7 +17,7 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Pat")
 	if err != nil {
 		log.Fatal(err)
 	}
--- tutorials/greetings/greetings.go
+++ tutorials/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the person provided
@@ -10,6 +11,19 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome - here's a greeting!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat returns a random message out of a set of messages.
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome",
+		"Great to see you %v",
+		"Hail %v, hello",
+		"Yo %v",
+		"Heyo matey %v",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
